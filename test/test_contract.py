470594611a9b51502026f383a915a0713403010b
dingxin <wdx7266@vip.qq.com>
2018-11-08 11:11:05 +0800

    update test

--- test/test_contract.py
+++ test/test_contract.py
@@ -12,14 +12,14 @@ from ontology.smart_contract.neo_contract.abi.abi_info import AbiInfo
 
 ontology = OntologySdk()
 remote_rpc_address = 'http://polaris3.ont.io:20336'
-contract_address_hex = '9026b8595d5c97a3ffb41d9c7aadaccfde82c40b'
+contract_address_hex = 'cf25ea1932ddbc9a03ce62131001d8bcdccc12ea'
 contract_address_bytearray = bytearray(binascii.a2b_hex(contract_address_hex))
 contract_address_bytearray.reverse()
 ontology.set_rpc(remote_rpc_address)
 root_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 wallet_path = os.path.join(root_folder, 'wallet', 'wallet.json')
 contracts_folder = os.path.join(root_folder, 'contracts')
-contracts_path = os.path.join(contracts_folder, 'interplanetary-album.json')
+contracts_path = os.path.join(contracts_folder, 'interplanetary-album.abi.json')
 with open(contracts_path) as f:
     contract_abi = json.loads(f.read())
     entry_point = contract_abi.get('entrypoint', '')
@@ -50,20 +50,21 @@ class TestSmartContract(unittest.TestCase):
         get_item_list_func.set_params_value((ont_id_acct.get_address().to_array(),))
         item_list = ontology.neo_vm().send_transaction(contract_address_bytearray, None, None, 0, 0, get_item_list_func,
                                                        True)
-        if item_list is None:
+        if item_list is None or None in item_list:
             item_list = list()
         for index in range(len(item_list)):
-            item_list[index][0] = binascii.a2b_hex(item_list[index][0]).decode('ascii')
-            item_list[index][1] = binascii.a2b_hex(item_list[index][1]).decode('ascii')
+            item_list[index][0] = binascii.a2b_hex(item_list[index][0])
+            item_list[index][1] = binascii.a2b_hex(item_list[index][1])
+            item_list[index][2] = binascii.a2b_hex(item_list[index][2])
+            item_list[index][3] = binascii.a2b_hex(item_list[index][3])
         print(item_list)
 
-    def test_del_item_list(self):
-        ipfs_address = 'QmaxL3ixbfG1mwQeKRdBPnYxqiESv5nkKg5UoppdvtZPfn'
-        del_one_item_func = abi_info.get_function('del_ont_item')
-        del_one_item_func.set_params_value((ont_id_acct.get_address().to_array(), ipfs_address))
+    def test_clear_item_list(self):
+        clear_item_list_func = abi_info.get_function('clear_item_list')
+        clear_item_list_func.set_params_value((ont_id_acct.get_address().to_array(),))
         tx_hash = ontology.neo_vm().send_transaction(contract_address_bytearray, ont_id_acct, acct, gas_limit,
-                                                     gas_price, del_one_item_func, False)
-        print(tx_hash)
+                                                     gas_price, clear_item_list_func, False)
+        self.assertEqual(64, tx_hash)
 
 
 if __name__ == '__main__':
